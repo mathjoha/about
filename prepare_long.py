a2eb39219a5a526d240d4633707a94329100e7c2
M~Johansson <MathiasJoha@gmail.com>
2024-01-12 15:55:47 +0100

    Pre-render short cv to create full

---
+++ prepare_long.py
@@ -0,0 +1,14 @@
+import os
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+short_file = os.path.join(root_dir, "cv", "short.qmd")
+with open(short_file, "r", encoding="utf8") as f:
+    content = f.read()
+
+contet = content.replace("max-items: 3", "")
+
+full_file = os.path.join(root_dir, "cv", "full.qmd")
+with open(full_file, "w", encoding="utf8") as f:
+    f.write('# Copied from "short.qmd"')
+    f.write(content)
