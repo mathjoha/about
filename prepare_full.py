803f9c4fda6087d48eb6721f5063d45b782e7cb6
M~Johansson <MathiasJoha@gmail.com>
2024-06-23 19:07:08 +0200

    Simplify structure through .yaml and .ejs

--- prepare_full.py
+++ prepare_full.py
@@ -6,7 +6,8 @@ short_file = os.path.join(root_dir, "cv", "short.qmd")
 with open(short_file, "r", encoding="utf8") as f:
     content = f.read()
 
-contet = content.replace("max-items: 3", "")
+content = content.replace("max-items: 3", "")
+
 
 full_file = os.path.join(root_dir, "cv", "full.qmd")
 with open(full_file, "w", encoding="utf8") as f:
